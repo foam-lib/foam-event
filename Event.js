e5a942789061756df5505ecd8abd22d972e38a78
Henryk Wollik <olafyoorsen@hotmail.com>
2016-06-03 22:36:16 +0200

    initial

---
+++ Event.js
@@ -0,0 +1,25 @@
+export default class Event{
+    constructor(type, data){
+        this._sender = null;
+        this._type   = type;
+        this._data   = data;
+
+        this._stopPropagation = false;
+    }
+
+    stopPropagation(){
+        this._stopPropagation = true;
+    }
+
+    get sender(){
+        return this._sender;
+    }
+
+    get type(){
+        return this._type;
+    }
+
+    get data(){
+        return this._data;
+    }
+}
